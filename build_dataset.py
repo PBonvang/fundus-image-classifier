89196236eb0ac33b59f3773274c3069115fa71b9
PBonvang <peter.bonvang@gmail.com>
2022-01-05 11:45:33 +0100

    Builder: Creates dataset with same amount of each class

--- build_dataset.py
+++ build_dataset.py
@@ -7,12 +7,32 @@ import shutil
 import os
 import pandas as pd
 
+def get_evenly_weigthed_classes_paths(image_paths):
+    fundus_paths = list()
+    non_fundus_paths = list()
+    for path in image_paths:
+        if path == 'nan':
+            continue
+
+        image_name = path.split(os.path.sep)[-1]
+        image_type = SAMPLE_TYPE.to_sample_type(image_name)
+        if image_type == SAMPLE_TYPE.FUNDUS:
+            fundus_paths.append(path)
+        else:
+            non_fundus_paths.append(path)
+
+    m = int(np.ceil(len(fundus_paths)/len(non_fundus_paths)))
+    non_fundus_paths = (non_fundus_paths*m)[:len(fundus_paths)]
+    evenly_weighted_paths = fundus_paths + non_fundus_paths
+
+    return evenly_weighted_paths
+
 def copy_images(image_paths, dest):
     # check if the destination folder exists and if not create it
     if os.path.exists(dest):
         clean = input("Destination folder already exists, would you like to erase existing images it before copying new images? [Y|N]:")
         
-        if clean.upper() == 'Y' or clean == "":
+        if clean.upper() == 'Y':
             shutil.rmtree(dest)
 
     if not os.path.exists(dest):
@@ -25,26 +45,30 @@ def copy_images(image_paths, dest):
         if not os.path.exists(full_sub_path):
             os.makedirs(full_sub_path)
 
+    i = 1
     for image_path in image_paths:
         if image_path == 'nan':
             continue
 
         image_name = image_path.split(os.path.sep)[-1]
+        name, ext = os.path.splitext(image_name)
         image_type = SAMPLE_TYPE.to_sample_type(image_name)
-        image_dest = os.path.join(dest, classes[image_type.value], image_name)
-        if not os.path.exists(image_dest):
-            shutil.copy(image_path, image_dest)
+        image_dest = os.path.join(dest, classes[image_type.value], f"{name}-{i}.{ext}")
+
+        shutil.copy(image_path, image_dest)
+        
+        i += 1
 
 print("[INFO] loading training data...")
-image_paths = list(paths.list_images(config.SOURCE_PATH)) #\
-    #[:1024]# Else the program is killed, due to limited memory (8 GB)
-np.random.shuffle(image_paths)
+image_paths = list(paths.list_images(config.SOURCE_PATH))
+evenly_weighted_paths = get_evenly_weigthed_classes_paths(image_paths)
+np.random.shuffle(evenly_weighted_paths)
 
-val_len = int(len(image_paths) * config.VAL_SPLIT)
-train_len = len(image_paths) - val_len
+val_len = int(len(evenly_weighted_paths) * config.VAL_SPLIT)
+train_len = len(evenly_weighted_paths) - val_len
 
-training_paths = image_paths[:train_len]
-validation_paths = image_paths[train_len:]
+training_paths = evenly_weighted_paths[:train_len]
+validation_paths = evenly_weighted_paths[train_len:]
 
 print("[INFO] copying training and test images...")
 copy_images(training_paths, config.TRAIN)
