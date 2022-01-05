deb36fb334903a6d8b53de1029b99fbe2f6b7891
PBonvang <peter.bonvang@gmail.com>
2022-01-05 18:08:06 +0100

    evaluator.py updated to use utils

--- evaluator.py
+++ evaluator.py
@@ -7,35 +7,12 @@ from numpy import vstack
 from numpy import argmax
 from sklearn.metrics import accuracy_score
 
-from Model import CNN
+from Model import Model
 import create_dataloader
+from utils.evaluation import evaluate_model
 
-def evaluate_model(test_dl, model):
-    predictions, actuals = list(), list()
-    for i, (inputs, targets) in enumerate(test_dl):
-        inputs = inputs.to(config.DEVICE)
-        targets = targets.to(config.DEVICE)
-
-        # evaluate the model on the test set
-        yhat = model(inputs)
-        # retrieve numpy array
-        yhat = yhat.detach().numpy()
-        actual = targets.numpy()
-        # convert to class labels
-        yhat = argmax(yhat, axis=1)
-        # reshape for stacking
-        actual = actual.reshape((len(actual), 1))
-        yhat = yhat.reshape((len(yhat), 1))
-        # store
-        predictions.append(yhat)
-        actuals.append(actual)
-    predictions, actuals = vstack(predictions), vstack(actuals)
-    # calculate accuracy
-    acc = accuracy_score(actuals, predictions)
-    return acc
-
-model = CNN(1)
-model_path = os.path.join(config.MODEL_PATH, "05_01_2022__11_28_34.pth")
+model = Model(1)
+model_path = os.path.join(config.MODELS_PATH, "05_01_2022__11_28_34.pth")
 model.load_state_dict(torch.load(model_path))
 model.eval()
 
