c7348548d83a8fa8d334cd3ea3e0db4fa705e921
PBonvang <peter.bonvang@gmail.com>
2022-01-07 14:57:38 +0100

    model validator

---
+++ utils/validation.py
@@ -0,0 +1,11 @@
+from utils.IModel import IModel
+
+def model_is_valid(model: IModel) -> bool:
+    return (
+        not model.loss_func == None
+        and not model.optimizer_func == None
+        and not model.epochs == None
+        and not model.batch_size == None
+        and not model.lr == None
+        and not model.optimizer == None
+    )
