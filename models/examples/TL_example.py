45704f95bfe108c24770d78a94f81becd607d2d5
PBonvang <peter.bonvang@gmail.com>
2022-01-21 00:11:54 +0100

    Issue#7: Examples cleaned

--- models/examples/TL_example.py
+++ models/examples/TL_example.py
@@ -1,35 +1,17 @@
 import torch
 
-from imutils import paths
-from torch.nn.modules.activation import Sigmoid
-from torch.nn.modules.loss import BCELoss
-from torchvision.models import resnet50
 import torchvision.models as models
 from torchvision import transforms
-from tqdm import tqdm
 from torch import nn
-import matplotlib.pyplot as plt
-import numpy as np
 
-from torch.nn import Conv2d
-from torch.nn import MaxPool2d
-from torch.nn import Linear
-from torch.nn import ReLU
-from torch.nn import Softmax
 from torch.nn import Module
-from torch.nn.init import kaiming_uniform_
-from torch.nn.init import xavier_uniform_
-from torch.nn import CrossEntropyLoss
 from torch.nn import BCEWithLogitsLoss
-from torch.optim import SGD
 from torch.optim import Adam
 
 from utils.IModel import IModel
 import config
 
-# DEFINE NETWORK HERE:
-
-
+# DEFINE NETWORK HERE
 class Network(Module):
     def __init__(self, n_channels):
         super(Network, self).__init__()
@@ -65,7 +47,7 @@ class Network(Module):
 # DEFINE MODEL HERE
 class Model(IModel):
     # SET MODEL ATTRIBUTES HERE:
-    loss_func = BCEWithLogitsLoss()
+    loss_func = BCEWithLogitsLoss(pos_weight=torch.tensor([config.DS_WEIGHT]).to(config.DEVICE))
     optimizer_func = Adam
     epochs = 10
     batch_size = 16
@@ -73,18 +55,14 @@ class Model(IModel):
 
     training_transforms = transforms.Compose([
         transforms.Resize(config.IMAGE_SHAPE),
-        # transforms.Grayscale(),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(90),
         transforms.ToTensor(),
-        #transforms.Normalize(mean=config.MEAN, std=config.STD)
     ])
 
     validation_transforms = transforms.Compose([
         transforms.Resize(config.IMAGE_SHAPE),
-        # transforms.Grayscale(),
         transforms.ToTensor(),
-        #transforms.Normalize(mean=config.MEAN, std=config.STD)
     ])
     # END MODEL ATTRIBUTES
 
